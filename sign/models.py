6ebdbc4aec27dc6b4d3839201e9577d195e6373c
ydzhincode <130305680+ydzhincode@users.noreply.github.com>
2023-09-09 11:29:08 +0300

    Add files via upload

---
+++ sign/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class BaseRegisterForm(UserCreationForm):
+    email = forms.EmailField(label = "Email")
+    first_name = forms.CharField(label = "Имя") # опционально, можно не указывать
+    last_name = forms.CharField(label = "Фамилия") # опционально
+
+    class Meta:
+        model = User
+        fields = ("username",
+                  "first_name", # опционально
+                  "last_name", # опционально
+                  "email",
+                  "password1",
+                  "password2", )
+
+# Create your models here.
